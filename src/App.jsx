2f3a7d868f6b8332a8df04ebb53f4cfda44dd885
Gayratov Javohir <javohirgayratov26@gmail.com>
2024-08-20 15:21:22 +0500

    something

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Container from "./components/Container/Container";
+import Layout from "./components/Layout/Layout";
+
+const App = () => {
+  return (
+    <div>
+      <Container>
+        <Layout>hello</Layout>
+      </Container>
+    </div>
+  );
+};
+
+export default App;
